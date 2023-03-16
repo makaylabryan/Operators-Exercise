4e23a83156a138f1c40d2247fb287cd377d57449
Makayla Bryan <makaylabryan@gmail.com>
2023-03-16 19:03:28 -0400

    operants exercise attempt 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace OperatorExercise
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
@@ -11,11 +14,21 @@
             int remainder = a % b;
 
             {
-                if (a == 17 && b == 4); 
+                if (a == 17 && b == 4) ;
                 {
                     Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+                    Console.WriteLine("What is the radius of your circle?");
+                    var radius = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle(radius)}");
                 }
             }
+        }
+        public static double AreaOfCircle(double radius)
+        {
+                var area = Math.PI * Math.Pow(radius, 2);
+                return area;
         }
     }
 }
